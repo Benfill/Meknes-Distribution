02d444569cad89a45c91c83f91c8eb9c34981c72
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-06-30 17:40:12 +0100

    Update index.jsx

--- app/src/pages/product/index.jsx
+++ app/src/pages/product/index.jsx
@@ -4,68 +4,11 @@ import Spinner from "../../components/Other/Spinner";
 import GET from "../../utils/GET";
 import { validateProduct } from "../../utils/validationFunctions";
 
-const columns = [
-  {
-    accessorKey: "id",
-    header: "Id",
-    enableEditing: false,
-  },
-  {
-    accessorKey: "designation",
-    header: "Designation",
-  },
-  {
-    accessorKey: "supplier_id",
-    header: "Fournisseur",
-  },
-  {
-    accessorKey: "sub_category_id",
-    header: "Group",
-  },
-  {
-    accessorKey: "marge_brut",
-    header: "Marge Brut",
-  },
-  {
-    accessorKey: "prix_achat",
-    header: "Prix Achat",
-  },
-  {
-    accessorKey: "prix_tarif",
-    header: "Prix Tarif",
-  },
-  {
-    accessorKey: "prix_vente",
-    header: "Prix Vente",
-  },
-  {
-    accessorKey: "prix_vente_net",
-    header: "Prix Vente Net",
-  },
-  {
-    accessorKey: "remise",
-    header: "Remise",
-  },
-  {
-    accessorKey: "TVA",
-    header: "TVA",
-  },
-  {
-    accessorKey: "reference",
-    header: "Reference",
-  },
-  {
-    accessorKey: "image",
-    header: "Image",
-    Cell: ({ cell }) => (
-      cell.value ? <img src={cell.value} alt="Product" style={{ width: '50px', height: '50px' }} /> : 'No Image'
-    ),
-  },
-];
 
 export const Product = () => {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [data, setData] = useState([]);
+  const [suppliers, setSuppliers] = useState([]);
 
   const fetchData = async () => {
     setLoading(true);
@@ -74,9 +17,37 @@ export const Product = () => {
     setLoading(false);
   };
 
+  const getSuppliers = async () => {
+    const r = await GET('suppliers');
+    if (r.message === 'success') {
+      setSuppliers(r.suppliers);
+    }
+  };
+
   useEffect(() => {
     fetchData();
+    getSuppliers();
   }, []);
+
+  const columns = [
+    { accessorKey: "id", header: "Id", enableEditing: false },
+    { accessorKey: "designation", header: "Designation" },
+    { accessorKey: "supplier_id", header: "Fournisseur"},
+    { accessorKey: "sub_category_id", header: "Group" },
+    { accessorKey: "marge_brut", header: "Marge Brut" },
+    { accessorKey: "prix_achat", header: "Prix Achat" },
+    { accessorKey: "prix_tarif", header: "Prix Tarif" },
+    { accessorKey: "prix_vente", header: "Prix Vente" },
+    { accessorKey: "prix_vente_net", header: "Prix Vente Net" },
+    { accessorKey: "remise", header: "Remise" },
+    { accessorKey: "TVA", header: "TVA" },
+    { accessorKey: "reference", header: "Reference" },
+    // { 
+    //   accessorKey: "image", 
+    //   header: "Image", 
+    //   Cell: ({ cell }) => ( cell.value ? <img src={cell.value} alt="Product" style={{ width: '50px', height: '50px' }} /> : 'No Image') 
+    // },
+  ];
 
   return loading ? (
     <Spinner /> 
@@ -91,6 +62,7 @@ export const Product = () => {
           columns={columns}
           entityType="Product"
           validateEntity={validateProduct}
+          suppliers={suppliers}
         />
       ) : (
         <p>No records.</p>
